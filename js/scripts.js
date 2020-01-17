3de27dff131b6fbed46c305b6269065fcfb570bc
Alex Skreen <skreenalex@gmail.com>
2020-01-17 11:35:25 -0800

    add console logs to verify that all variables in both forms work.

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,32 @@
 $(document).ready(function(){
-    $("form#celebrityForm").submit(function(event) {
+    $("form#languageForm").submit(function(event) {
         var experience = $("#experience").val();
         var designorBuild = $("#designorBuild").val();
         var frontEndBackEnd = $("#frontEndBackEnd").val();
         var longForm = $("#longForm").val();
         var question5 = $("#question5").val();
         var question6 = $("#question6").val();
+        console.log(experience, designorBuild, frontEndBackEnd, longForm, question5, question6);
 
 
 
-        if (experience === "1" && designorBuild === "1" && frontEndBackEnd === "3" && longForm === "3" && question5 === "3") {
+        if (experience === "1" && designorBuild === "1" && frontEndBackEnd === "1" && longForm === "2" && question5 === "3" && question6 === "3") {
             $("#two, #three").hide();
             $("#four, #five").hide();
             $("#one").show();
-          } else if (experience === "1" && designorBuild === "1" && frontEndBackEnd === "3" && longForm === "3" && question5 === "3") {
+          } else if (experience === "1" && designorBuild === "1" && frontEndBackEnd === "3" && longForm === "3" && question5 === "3" && question6 === "3") {
             $("#one, #three").hide();
             $("#four, #five").hide();
             $("#two").show();
-          } else if (experience === "1" && designorBuild === "1" && frontEndBackEnd === "3" && longForm === "3" && question5 === "3") {
+          } else if (experience === "1" && designorBuild === "1" && frontEndBackEnd === "3" && longForm === "3" && question5 === "3" && question6 === "3") {
             $("#one, #two").hide();
             $("#four, #five").hide();
             $("#three").show();
-          } else if (experience === "1" && designorBuild === "1" && frontEndBackEnd === "3" && longForm === "3" && question5 === "3") {
+          } else if (experience === "1" && designorBuild === "1" && frontEndBackEnd === "3" && longForm === "3" && question5 === "3" && question6 === "3") {
             $("#one, #two").hide();
             $("#three, #five").hide();
             $("#four").show();
-          } else if (experience === "1" && designorBuild === "1" && frontEndBackEnd === "3" && longForm === "3" && question5 === "3") {
+          } else if (experience === "1" && designorBuild === "1" && frontEndBackEnd === "3" && longForm === "3" && question5 === "3" && question6 === "3") {
             $("#one, #two").hide();
             $("#four, #three").hide();
             $("#five").show();
@@ -33,4 +34,11 @@ $(document).ready(function(){
           event.preventDefault();
       
         });
+    $("form#contactMeForm").submit(function(event) {
+      var firstName = $("input#firstName").val();
+      var lastName = $("input#lastName").val();
+      var anyQuestions = $("textarea#anyQuestions").val();
+      console.log(firstName, lastName, anyQuestions);
+    event.preventDefault();
+});
 });
