5d2e67c727fb693827cc178e1ae15bbdfd337bb2
Alex Skreen <skreenalex@gmail.com>
2020-01-17 14:58:20 -0800

    create id for contact me form that shows up when they submit their info. Also created a prompt so we can either reveal the id or show the user a prompt

--- js/scripts.js
+++ js/scripts.js
@@ -4,25 +4,25 @@ $(document).ready(function(){
         var designorBuild = $("#designorBuild").val();
         var frontEndBackEnd = $("#frontEndBackEnd").val();
         var longForm = $("#longForm").val();
-        var question5 = $("#question5").val();
-        var question6 = $("#question6").val();
-        // console.log(experience, designorBuild, frontEndBackEnd, longForm, question5, question6);
+        var location = $("#location").val();
+        var workSchedule = $("#workSchedule").val();
+        console.log(experience, designorBuild, frontEndBackEnd, longForm, location, workSchedule);
 
 
 
-        if (experience === "1" && designorBuild === "1" && frontEndBackEnd === "1" && longForm === "1" && question5 === "1" && question6 === "1") {
+        if (experience === "1" && designorBuild === "1" && frontEndBackEnd === "1" && longForm === "1" && location === "1" && workSchedule === "1") {
             $("#two, #three, #four, #five").hide();
             $("#one").show();
-          } else if (experience === "3" && designorBuild === "2" && frontEndBackEnd === "2" && longForm === "2" && question5 === "1" && question6 === "1") {
+          } else if (experience === "3" && designorBuild === "2" && frontEndBackEnd === "2" && longForm === "2" && location === "1" && workSchedule === "1") {
             $("#one, #three, #four, #five").hide();
             $("#two").show();
-          } else if (experience === "2" && designorBuild === "1" && frontEndBackEnd === "3" && longForm === "2" && question5 === "1" && question6 === "1") {
+          } else if (experience === "2" && designorBuild === "1" && frontEndBackEnd === "3" && longForm === "2" && location === "1" && workSchedule === "1") {
             $("#one, #two, #four, #five").hide();
             $("#three").show();
-          } else if (experience === "1" && designorBuild === "1" && frontEndBackEnd === "1" && longForm === "2" && question5 === "1" && question6 === "1") {
+          } else if (experience === "1" && designorBuild === "1" && frontEndBackEnd === "1" && longForm === "2" && location === "1" && workSchedule === "1") {
             $("#one, #two, #three, #five").hide();
             $("#four").show();
-          } else if (experience === "4" && designorBuild === "2" && frontEndBackEnd === "2" && longForm === "2" && question5 === "1" && question6 === "1") {
+          } else if (experience === "4" && designorBuild === "2" && frontEndBackEnd === "2" && longForm === "2" && location === "1" && workSchedule === "1") {
             $("#one, #two, #four, #three").hide();
             $("#five").show();
           }
@@ -33,7 +33,13 @@ $(document).ready(function(){
       var firstName = $("input#firstName").val();
       var lastName = $("input#lastName").val();
       var anyQuestions = $("textarea#anyQuestions").val();
-      // console.log(firstName, lastName, anyQuestions);
+      console.log(firstName, lastName, anyQuestions);
+      $("#six").show();
+      $("#firstNameForm").text(firstName);
+      $("#lastNameForm").text(lastName);
+      // alert("Thank you" + " " firstName + " " + lastName + ". One of our teachers will reach out to you shortly! Thank you for taking the time to fill out our contact me form.")
+
+      
     event.preventDefault();
 });
 });
