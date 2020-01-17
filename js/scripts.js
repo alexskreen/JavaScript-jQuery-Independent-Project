bab88606d34486e7392238fcbb8bcb4f66dca516
Alex Skreen <skreenalex@gmail.com>
2020-01-17 14:25:45 -0800

    simplified JS to remove redundant lines within the else if section

--- js/scripts.js
+++ js/scripts.js
@@ -6,28 +6,24 @@ $(document).ready(function(){
         var longForm = $("#longForm").val();
         var question5 = $("#question5").val();
         var question6 = $("#question6").val();
-        console.log(experience, designorBuild, frontEndBackEnd, longForm, question5, question6);
+        // console.log(experience, designorBuild, frontEndBackEnd, longForm, question5, question6);
 
 
 
         if (experience === "1" && designorBuild === "1" && frontEndBackEnd === "1" && longForm === "1" && question5 === "1" && question6 === "1") {
             $("#two, #three, #four, #five").hide();
             $("#one").show();
-          } else if (experience === "1" && designorBuild === "2" && frontEndBackEnd === "2" && longForm === "2" && question5 === "1" && question6 === "1") {
-            $("#one, #three").hide();
-            $("#four, #five").hide();
+          } else if (experience === "3" && designorBuild === "2" && frontEndBackEnd === "2" && longForm === "2" && question5 === "1" && question6 === "1") {
+            $("#one, #three, #four, #five").hide();
             $("#two").show();
-          } else if (experience === "2" && designorBuild === "1" && frontEndBackEnd === "3" && longForm === "3" && question5 === "3" && question6 === "3") {
-            $("#one, #two").hide();
-            $("#four, #five").hide();
+          } else if (experience === "2" && designorBuild === "1" && frontEndBackEnd === "3" && longForm === "2" && question5 === "1" && question6 === "1") {
+            $("#one, #two, #four, #five").hide();
             $("#three").show();
-          } else if (experience === "1" && designorBuild === "1" && frontEndBackEnd === "3" && longForm === "3" && question5 === "3" && question6 === "3") {
-            $("#one, #two").hide();
-            $("#three, #five").hide();
+          } else if (experience === "1" && designorBuild === "1" && frontEndBackEnd === "1" && longForm === "2" && question5 === "1" && question6 === "1") {
+            $("#one, #two, #three, #five").hide();
             $("#four").show();
-          } else if (experience === "1" && designorBuild === "1" && frontEndBackEnd === "3" && longForm === "3" && question5 === "3" && question6 === "3") {
-            $("#one, #two").hide();
-            $("#four, #three").hide();
+          } else if (experience === "4" && designorBuild === "2" && frontEndBackEnd === "2" && longForm === "2" && question5 === "1" && question6 === "1") {
+            $("#one, #two, #four, #three").hide();
             $("#five").show();
           }
           event.preventDefault();
@@ -37,7 +33,7 @@ $(document).ready(function(){
       var firstName = $("input#firstName").val();
       var lastName = $("input#lastName").val();
       var anyQuestions = $("textarea#anyQuestions").val();
-      console.log(firstName, lastName, anyQuestions);
+      // console.log(firstName, lastName, anyQuestions);
     event.preventDefault();
 });
 });
